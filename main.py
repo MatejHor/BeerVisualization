ff6c8cefc543f17857245922591f3ac4991e433b
MatejHor <32205201+MatejHor@users.noreply.github.com>
2021-01-29 22:23:11 +0100

    Update html Title

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ import dash_html_components as html
 from dash.dependencies import Input, Output
 
 app = dash.Dash(__name__)
+app.title = 'Craft Beer visualization'
 server = app.server
 
 # ------------------------------------------------------------------------------
