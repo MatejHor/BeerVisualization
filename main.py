87e9287921c3c73f42b5d0c80fb336ac76c789d2
MatejHor <32205201+MatejHor@users.noreply.github.com>
2021-01-29 22:25:31 +0100

    Update icon

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ import dash_html_components as html
 from dash.dependencies import Input, Output
 
 app = dash.Dash(__name__)
-app.title = 'Craft Beer visualization'
+app.title = 'Craft Beers'
 server = app.server
 
 # ------------------------------------------------------------------------------
